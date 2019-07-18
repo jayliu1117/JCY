69d2d02de8dd4a074d1066ed6f89606a7aeacdd6
siyuant2 <31710424+siyuant2@users.noreply.github.com>
2019-07-17 22:35:02 -0500

    all the CURDs done

--- cs411projectJCY/Signup/urls.py
+++ cs411projectJCY/Signup/urls.py
@@ -5,4 +5,10 @@ app_name = 'Signup'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('addperson/', views.addperson, name = 'addperson'),
+    path('changepassword/', views.changepassword, name = 'changepassword'),
+    path('updatepassword/', views.updatepassword, name = 'updatepassword'),
+    path('deactivate_account/', views.deactivate_account, name= 'deactivate_account'),
+    path('delete_account/', views.delete_account, name = 'delete_account'),
+    path('forgetpassword/', views.forgetpassword, name = 'forgetpassword'),
+    path('search_password', views.search_password, name = 'search_password')
     ]
